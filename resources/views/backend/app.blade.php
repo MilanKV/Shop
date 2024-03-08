681ede82ef9099f42e6a6a40d2eef6964ac25ae5
MilanKV <milanlemonh@gmail.com>
2024-03-08 23:58:41 +0100

    feat: Implement initial sidebar design for admin panel - Created a modular sidebar layout - Links and sublinks for primary admin functionalities - Clear and modular layout structure - Included separate sections for head, sidebar, header, content, and footer

---
+++ resources/views/backend/app.blade.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+{{-- Head --}}
+@include('backend.includes.head')
+
+<body class="d-flex flex-column min-vh-100">
+    {{-- Sidebar --}}
+    @include('backend.includes.sidebar')
+
+
+    <div class="page-section">
+        {{-- Header --}}
+        {{-- @include('backend.includes.header') --}}
+
+        <div class="main">
+            {{-- Page Content --}}
+            @yield('content')
+        </div>
+
+        {{-- Footer --}}
+        {{-- @include('backend.includes.footer') --}}
+    </div>
+    {{-- app-body --}}
+
+    {{-- Scripts --}}
+    @vite('resources/js/app.js')
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\admin\AdminController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('app');
 });
+
+Route::get('/admin', [AdminController::class, 'index'])->name('admin');
