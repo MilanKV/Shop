a839f369652203eace6e131317b71be3a59a9e2a
MilanKV <milanlemonh@gmail.com>
2024-06-01 17:25:25 +0200

    Added Home.vue section with dynamic design elements, including a visually appealing pulsing circle effect behind a headphones image on the right side and a clear heading with a call-to-action button on the left side.

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,3 +1,19 @@
 <template>
-    <h1>Welcome Home</h1>
+    <section class="Hero">
+        <div class="row">
+            <div class="left col-xl-6 col-lg-6">
+                <span class="small-title">Elevate Your <br> <span class="bold-text">Audio</span> Experience</span>
+                <h3 class="title">Unmatched <br> Sound Quality.</h3>
+                <a href="/shop" class="btn btn-outline">
+                    Shop Now
+                    <img src="../../img/icons/arrow-right.svg" alt="arrow-right">
+                </a>
+            </div>
+            <div class="right col-xl-6 col-lg-6">
+                <span class="circle"></span>
+                <span class="circle"></span>
+                <img src="../../img/Images/Headphones.png" alt="Headphone" class="img-responsive">
+            </div>
+        </div>
+    </section>
 </template>
