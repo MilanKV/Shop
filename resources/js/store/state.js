f8ab90c7c0e99d5c282e3c13ec10d8dd50b5e885
MilanKV <milanlemonh@gmail.com>
2024-06-30 23:29:23 +0200

    added pagination functionality to Shop
    - Implemented Pagination component to display page navigation with active page highlighting
    - Updated Shop to integrate Pagination component, manage product fetching based on selected filters (brand, color, price), and adjust pagination logic to show/hide prev/next navigation appropriately.

--- resources/js/store/state.js
+++ resources/js/store/state.js
@@ -5,4 +5,12 @@ export default {
     sortingOption: 'Low to High',
     products: [],
     brands: [],
+    pagination: {
+        total: 0,
+        per_page: 9,
+        current_page: 1,
+        last_page: 1,
+        from: 0,
+        to: 0,
+    },
 };
