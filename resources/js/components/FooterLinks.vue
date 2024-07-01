1f2ca3c5ccf38b1343da35a232393cdf417dc0bc
MilanKV <milanlemonh@gmail.com>
2024-07-01 23:28:41 +0200

    Added footer component and created reusable components: FooterLinks, ContactInfo, and PaymentMethods.
     Footer.vue:
     - Integrated sections for logo, company, store, support, contact info, copyrights, and payment methods.
    - Reused modular components: Logo, SocialLinks, FooterLinks, ContactInfo, PaymentMethods.
    
     FooterLinks.vue:
     - Created a reusable component for displaying titled link sections dynamically.
    
    ContactInfo.vue:
    - Added a component to showcase contact details with clickable phone numbers and emails.
    
    Payments.vue:
       - Designed a component to showcase accepted payment method icons.

---
+++ resources/js/components/FooterLinks.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="footer-section">
+        <h3 class="title">{{ title }}</h3>
+        <div class="content">
+            <ul>
+                <li v-for="link in links" :key="link.text">
+                    <a :href="link.href">{{ link.text }}</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FooterLinks',
+    props: {
+        title: {
+            type: String,
+            required: true
+        },
+        links: {
+            type: Array,
+            required: true
+        }
+    }
+};
+</script>
--- resources/js/layout/Default.vue
+++ resources/js/layout/Default.vue
@@ -2,21 +2,23 @@
     <header>
         <Header />
     </header>
-     <main>
-         <router-view/>
-     </main>
-     <footer>
-        <!-- <Footer /> -->
-     </footer>
+    <main>
+        <router-view />
+    </main>
+    <footer>
+        <Footer />
+    </footer>
 </template>
 
 <script>
 import Header from '../components/Header.vue'
+import Footer from '../components/Footer.vue'
 
 export default {
     name: "Default",
     components: {
         Header,
+        Footer,
     }
 }
 </script>
