325ea292d5842c70001015515bdb6d273aea64ed
MilanKV <milanlemonh@gmail.com>
2024-07-02 22:06:06 +0200

    Implemented "Back to Top" button functionality
    - Added a BackToTop.vue component to display a button that appears when the user scrolls down 200 pixels from the top.
    - Integrated scroll event listener to toggle visibility of the button based on scroll position.
    - Applied smooth scroll behavior to return to the top of the page when the button is clicked.
    - Styled button with SCSS for visual consistency and user interaction.

---
+++ resources/js/components/BackToTop.vue
@@ -0,0 +1,35 @@
+<template>
+    <div class="back-to-top" :class="{ show: shouldShowBackToTop }">
+        <button id="back-to-top" class="back-to-top-button" type="button" @click="scrollToTop">
+            <img src="../../img/icons/arrow-up.svg">
+        </button>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'BackToTop',
+    data() {
+        return {
+            shouldShowBackToTop: false
+        };
+    },
+    methods: {
+        scrollToTop() {
+            window.scrollTo({
+                top: 0,
+                behavior: "smooth"
+            });
+        },
+        handleScroll() {
+            this.shouldShowBackToTop = window.scrollY > 200;
+        }
+    },
+    mounted() {
+        window.addEventListener("scroll", this.handleScroll);
+    },
+    beforeDestroy() {
+        window.removeEventListener("scroll", this.handleScroll);
+    }
+};
+</script>
--- resources/js/components/SortingDropdown.vue
+++ resources/js/components/SortingDropdown.vue
@@ -2,10 +2,12 @@
     <div class="sorting" @click="toggleDropdown">
         <div class="title">
             <span class="current">{{ currentSorting }}</span>
-            <img :class="{ 'rotated': dropdownActive }" src="../../img/icons/chevron-down.svg">
+            <img :class="{ 'rotated': dropdownActive }" src="../../img/icons/arrow-down.svg">
         </div>
         <ul class="submenu" :class="{ active: dropdownActive }">
-            <li v-for="option in sortingOptions" :key="option" :class="{ selected: option === currentSorting, focus: option === currentSorting }" class="option" @click.stop="selectOption(option)">
+            <li v-for="option in sortingOptions" :key="option"
+                :class="{ selected: option === currentSorting, focus: option === currentSorting }" class="option"
+                @click.stop="selectOption(option)">
                 {{ option }}
             </li>
         </ul>
