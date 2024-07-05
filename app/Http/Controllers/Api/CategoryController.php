bf7aec2a9c37f1bde18ef12f1b321b0e1ae6b912
MilanKV <milanlemonh@gmail.com>
2024-07-05 17:19:26 +0200

    feat: Integrated Swiper.js for responsive category slider in Home.vue
    - Installed Swiper.js and dependencies, imported CSS for styling, and updated component structure.
    - Wrapped category items inside Swiper slides, configured Swiper with custom settings (slides-per-view, space-between).
    - Integrated Swiper event handlers for onSwiper and onSlideChange.
    - Implemented responsive breakpoints for Swiper component in Home.vue using Swiper.js.
    - Added scrollBehavior function in router configuration to ensure pages scroll to top on navigation.
    - Updated Home.vue to pass selected category ID to Shop page on category click.
    - Modified Shop.vue to set selected category based on query parameter and scroll to top on load.
    - Styled Category slider with SCSS for visual consistency and user interaction.
    - Added `getFilterParams()` method to centralize the retrieval of filter parameters (`selectedBrand`, `selectedColor`, `selectedPrice`, `selectedCategory`, `selectedSubcategory`).

--- app/Http/Controllers/Api/CategoryController.php
+++ app/Http/Controllers/Api/CategoryController.php
@@ -5,13 +5,17 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\Models\Category;
 use App\Http\Resources\CategoryResource;
-use Illuminate\Http\Request;
 
 class CategoryController extends Controller
 {
     public function index()
     {
         $categories = Category::with(['parentCategory', 'subcategories'])->where('is_parent', true)->get();
+
+        $categories->map(function ($category) {
+            $category->image_url = asset('storage/backend/category/category_images' . $category->category_image);
+            return $category;
+        });
 
         return response()->json(CategoryResource::collection($categories));
     }
--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class="Hero">
+    <section class="hero-section">
         <div class="row">
             <div class="left col-xl-6 col-lg-6">
                 <span class="small-title">Elevate Your <br> <span class="bold-text">Audio</span> Experience</span>
@@ -16,4 +16,99 @@
             </div>
         </div>
     </section>
+    <section class="category-section">
+        <div class="container">
+            <div class="row">
+                <div class="category-slider col-xxl-12">
+                    <swiper class="slider-horizontal swiper-container" :modules="modules" :breakpoints="breakpoints"
+                        :slides-per-view="slidesPerView" :space-between="20" :scrollbar="{ draggable: true }"
+                        @swiper="onSwiper" @slideChange="onSlideChange">
+                        <swiper-slide class="category-item " v-for="(category, index) in categories" :key="index">
+                            <div class="category-image">
+                                <a href="#" class="image" @click.prevent="goToShopWithCategory(category.id)">
+                                    <img :src="getImageUrl(category)" alt="">
+                                </a>
+                            </div>
+                            <div class="category-content">
+                                <h3 class="category-title">
+                                    <a href="#" class="title" @click.prevent="goToShopWithCategory(category.id)">{{
+                        category.category_name }}</a>
+                                </h3>
+                            </div>
+                        </swiper-slide>
+                    </swiper>
+                </div>
+            </div>
+        </div>
+    </section>
 </template>
+
+<script>
+import { mapState, mapActions } from 'vuex';
+import { Swiper, SwiperSlide } from 'swiper/vue';
+import { Scrollbar } from 'swiper/modules';
+
+import "swiper/css";
+import 'swiper/css/scrollbar';
+
+export default {
+    name: 'Home',
+    components: {
+        Swiper,
+        SwiperSlide,
+    },
+    data() {
+        return {
+            modules: [Scrollbar],
+            slidesPerView: 4,
+            breakpoints: {
+                // when window width is >= 320px
+                320: {
+                    slidesPerView: 1,
+                },
+                // when window width is >= 480px
+                480: {
+                    slidesPerView: 2,
+                },
+                // when window width is >= 640px
+                640: {
+                    slidesPerView: 2,
+                },
+                // when window width is >= 1024px
+                1024: {
+                    slidesPerView: 3,
+                },
+                // when window width is >= 1440px
+                1440: {
+                    slidesPerView: 4,
+                }
+            }
+        };
+    },
+    computed: {
+        ...mapState({
+            categories: state => state.categories,
+        }),
+
+    },
+    methods: {
+        ...mapActions(['fetchCategories']),
+
+        getImageUrl(category) {
+            return `/storage/${category.category_image}`;
+        },
+        onSwiper(swiper) {
+            // console.log(swiper);
+        },
+        onSlideChange() {
+            // console.log('slide change');
+        },
+        goToShopWithCategory(categoryId) {
+            this.$router.push({ name: 'Shop', query: { category: categoryId } });
+        },
+    },
+    created() {
+        this.fetchCategories();
+    }
+}
+</script>
